6d45d079f6282494c0e5a6d323b63c6d72451880
quanghoatrinh <quanghoa.trinh@gmail.com>
2024-08-08 11:18:51 +0700

    first commit

--- T2305M_API/Program.cs
+++ T2305M_API/Program.cs
@@ -1,6 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
+// Add CORS policy access
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        //policy.WithOrigins("http://localhost:3000");
+        policy.AllowAnyOrigin();
+        //policy.WithMethods("POST");
+        policy.AllowAnyMethod();
+        policy.AllowAnyHeader();
+    });
+});
+
 // connect db
 T2305M_API.Entities.T2305mApiContext.ConnectionString = builder.Configuration.GetConnectionString("T2305M_API");
 builder.Services.AddDbContext<T2305M_API.Entities.T2305mApiContext>(
@@ -22,6 +35,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
